32a4e65cbde0d02f1361fb1ac2ac4298682518be
Federico Menozzi <federicogmenozzi@gmail.com>
2016-03-18 09:43:51 -0400

    Make main.rs cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 extern crate dinky;
 
-use dinky::ppm::PPMImage as PPMImage;
-use dinky::color::Color as Color;
+use dinky::ppm::PPMImage;
+use dinky::color::Color;
 
 use std::path::Path;
 
 fn main() {
     let mut image = PPMImage::new(256, 256);
 
-    let red = Color::new(255,0,0);
-    image.set(2, 2, &red);
-
-    let path = Path::new("out.ppm");
-    image.write(&path);
+    image.set(2, 2, &Color::new(255, 0, 0));
+    image.write(&Path::new("out.ppm"));
 }
